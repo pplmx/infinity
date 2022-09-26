5e684dc0b2d0f6a26644d2982a83ce30ff46af6b
Mystic <215104920@qq.com>
2022-09-26 10:15:38 +0800

    feat(path): support *nix and win

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
-#include <iostream>
 #include "config_parser.h"
+#include <iostream>
+#include <cstdlib>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    ConfigParser::load(R"(C:\Users\pplmx\JetBrains\CLionProjects\infinity\CMakeLists.txt)");
+    const char *home = getenv("HOME");
+    auto p = fs::path(home) / fs::path("JetBrains/CLionProjects/infinity/CMakeLists.txt");
+    try {
+        p = canonical(p);
+    } catch (const std::exception &ex) {
+        printf("Canonical parser threw exception: %s.", ex.what());
+    }
+    ConfigParser::load(p.make_preferred().c_str());
     return 0;
 }
-
